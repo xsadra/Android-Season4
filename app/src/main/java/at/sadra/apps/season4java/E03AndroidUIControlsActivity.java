f1af5bc209f756c84f817369c3af8f6ab52de2f0
Sadra <ti9mail@gmail.com>
2018-08-12 23:04:35 +0200

    E03 Android UI Controls Activity

--- app/src/main/java/at/sadra/apps/season4java/E03AndroidUIControlsActivity.java
+++ app/src/main/java/at/sadra/apps/season4java/E03AndroidUIControlsActivity.java
@@ -2,6 +2,7 @@ package at.sadra.apps.season4java;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class E03AndroidUIControlsActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class E03AndroidUIControlsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_e03_android_uicontrols);
+        setTitle(getResources().getString(R.string.E03AndroidUIControlsActivity));
+
+        Button myButton = ((Button) findViewById(R.id.button1));
+
+        myButton.setText(R.string.newName);
+        myButton.setTextColor(getResources().getColor(R.color.buttonColor));
     }
+
+
 }
