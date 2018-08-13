1b5b3ba3c35c31feba554a0d11eea176dc22db0e
Sadra <ti9mail@gmail.com>
2018-08-14 00:31:17 +0200

    E06 Android Button

---
+++ app/src/main/java/at/sadra/apps/season4java/E06AndroidButtonActivity.java
@@ -0,0 +1,17 @@
+package at.sadra.apps.season4java;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static at.sadra.apps.season4java.R.string.E06AndroidButtonActivity;
+
+public class E06AndroidButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_e06_android_button);
+
+        setTitle(E06AndroidButtonActivity);
+    }
+}
