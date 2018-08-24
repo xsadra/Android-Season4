34fc4013be6d703b2b2aeb7fdda75e1c12a510fb
Sadra <31988724+xsadra@users.noreply.github.com>
2018-08-24 12:16:56 +0200

    E8-1 Android Edit Text

---
+++ app/src/main/java/at/sadra/apps/season4java/E08AndroidEditText.java
@@ -0,0 +1,14 @@
+    package at.sadra.apps.season4java;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class E08AndroidEditText extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_e08_android_edit_text);
+        setTitle(R.string.E08AndroidEditText);
+    }
+}
